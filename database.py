f0cd8b7dbd1f2883967e33a0e8936abdd3d0309e
Dipin-D <102004858+Dipin-D@users.noreply.github.com>
2023-10-24 21:39:10 +0000

    new update with secret key

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
+import os
 from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://wf5ojfa99wphg56c2lr5:pscale_pw_3WqOz6FRTPcfXfVZr6xgehvF1wfEYoV2oOT45ZVSdxj@aws.connect.psdb.cloud/portfolio?charset=utf8mb4",
-                      connect_args={
-                        "ssl": {
-                          "ssl_ca": "/etc/ssl/cert.pem"
-                        }
-                      })
+my_secret = os.environ['password']
+
+db_url = f"mysql+pymysql://09s5bxg4wen2627y77py:{my_secret}@aws.connect.psdb.cloud/portfolio?charset=utf8mb4"
+
+engine = create_engine(db_url, connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}})
 
 def db():
     with engine.connect() as conn:
@@ -15,16 +15,15 @@ def db():
         for row in result:
             job = row._asdict()
             jobs.append(job)
-          
+
         return jobs
-      
+
 def insert_contact(name, email, message):
-      with engine.connect() as conn:
-          query = text("INSERT INTO contacts (name, email, message) VALUES (:name, :email, :message)")
-          conn.execute(query, {"name": name, "email": email, "message": message})
+    with engine.connect() as conn:
+        query = text("INSERT INTO contacts (name, email, message) VALUES (:name, :email, :message)")
+        conn.execute(query, {"name": name, "email": email, "message": message})
 
-          return "Form submitted successfully!"
-
+    return "Form submitted successfully!"
 
 if __name__ == "__main__":
     jobs = db()
